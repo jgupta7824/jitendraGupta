e655b7b4b97dfd4d165b83043d594a90104115b9
Jitendra Gupta <er.jgupta2gmail.com>
2017-01-06 11:30:41 +0530

    commit----

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,25 +12,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-// ngAfterViewInit() {
-//   // You can also use "$(window).load(function() {"
-//             $(function () {
-//               // Slideshow 4
-//               $("#slider3").responsiveSlides({
-//               auto: true,
-//               pager:true,
-//               nav:false,
-//               speed: 500,
-//               namespace: "callbacks",
-//               before: function () {
-//                 $('.events').append("<li>before event fired.</li>");
-//               },
-//               after: function () {
-//                 $('.events').append("<li>after event fired.</li>");
-//               }
-//               });
-//
-//             });
-// }
 
 }
